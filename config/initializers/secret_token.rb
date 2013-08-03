eaefd093d5d049e9c216c6eddfbad863c9afeb3d
nate dawg <nglass1988@gmail.com>
2013-08-03 13:43:47 -0700

    modified git ignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Michaelhartl::Application.config.secret_key_base = '945437422b5ba9cdee167b6f220baffb690ab8396a86dece2a9ec8ef65ab2f71188b7c013e3cc30e4fe0f437d5f9955bc6f9b2aebf6e687c0584357f5ef46b7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Michaelhartl::Application.config.session_store :cookie_store, key: '_michaelhartl_session'

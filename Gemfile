a0d9e90ac46ae1b86807cf101eb48582f6f7daa2
nate dawg <nglass1988@gmail.com>
2013-08-04 14:15:54 -0700

    d3 stuff

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ ruby '2.0.0'
 #ruby-gemset=railstutorial_rails_4_0
 
 gem 'rails', '4.0.0'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'd3-rails'
 
 group :development, :test do
   gem 'sqlite3', '1.3.7'
---
+++ app/assets/javascripts/d3trial.js
@@ -0,0 +1,61 @@
+var data = [10, 50, 90, 130, 170];
+		var width = 500;
+		var height = 1000;
+
+		
+		
+
+		var widthscale = d3.scale.linear()
+						.domain([0, 170])
+						.range([0, width]);
+
+		var color = d3.scale.linear()
+					.domain([0, 170])
+					.range(['red', 'blue']);
+		var color1 = d3.scale.linear()
+					.domain([0, 170])
+					.range(['blue', 'red']);
+
+		var axis = d3.svg.axis()
+					.scale(widthscale);
+
+		var canvas = d3.select("body")
+				.append("svg")
+				.attr("width", width)
+				.attr("height", height)
+				.append('g')
+				.attr("transform", "translate(10, 10)");
+
+		var grad = canvas.append("defs").append("radialGradient").attr("id", "grad")
+              .attr('cx', '50%').attr('cy', '50%').attr('r', '50%');
+				grad.append("stop").attr("offset", "0%").attr("stop-color", "lightblue").attr('stop-opacity', 1);
+				grad.append("stop").attr("offset", "100%").attr("stop-color", "white").attr('stop-opacity', 1);
+
+		var circles = canvas.selectAll('circle')
+					.data(data)
+					.enter()
+					.append('circle')
+					.attr('cx', function(d) {return widthscale(d);})
+					.attr('cy', function(d) {return widthscale(d);})
+					.attr('r', function(d) {return widthscale(Math.sqrt(d));})
+					.attr('fill', 'url(#grad)');
+
+
+		circles.transition()
+						.duration(1500)
+						.delay(1000)
+						.attr("cx", function(d) {return d })
+						.attr("cy", function(d) { return d })
+						.style('fill', function(d) { return color1(d)})
+						.each("end", function() { d3.select(this)
+						.transition()
+						.duration(1500)
+						.delay(3000)
+						.attr("cx", function(d) {return d * 3 })
+						.attr("cy", function(d) { return d * 3})
+						.style('fill', function(d) { return color(d); }); });
+
+
+		canvas.append('g').attr("transform", "translate(0, 600)").call(axis);
+
+		

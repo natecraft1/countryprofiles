e1c0424ac2e32bc09fbace80eb5131b4a840bc42
nate dawg <nglass1988@gmail.com>
2013-08-05 00:28:10 -0700

    enddy ballin

--- app/assets/javascripts/d3trial.js
+++ app/assets/javascripts/d3trial.js
@@ -13,20 +13,38 @@ var path = d3.geo.path()
 
 
 
+
+
+$("#target").submit(function() {
+  var result = $('#myInput').val();   
 // load and display the World
 d3.json("/assets/worldcountries.geo.json", function(topology) {
+ 
+    for (i=0; i < topology.features.length; i++)
+    {
+      if (topology.features[i].properties.name == result)
+        { var found = topology.features[i].geometry; }
+    }
+    // var cuntname = topology.features.properties.name("Afghanistan");
     // var zoom = d3.behavior.zoom()
     //   .translate([0, 0])
     //   .scale(1)
     //   .scaleExtent([1, 8])
     //   .on("zoom", zoomed);
     svg.selectAll("country")
-      .data([topology.features[0]])
+      .data([found])
     .enter()
       .append("path")
       .attr("class", "country")
       .attr("d", path);
 });
+return false;
+ });
+
+
+
+
+
 function sizeChange() {
       d3.select("g").attr("transform", "scale(" + $("#container").width()/900 + ")");
       $("svg").height($("#container").width()*0.618);

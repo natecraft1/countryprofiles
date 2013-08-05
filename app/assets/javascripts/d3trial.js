3980572fa5a4a6968113cce905b8f086ab680be4
nate dawg <nglass1988@gmail.com>
2013-08-04 22:29:12 -0700

    window adjust

--- app/assets/javascripts/d3trial.js
+++ app/assets/javascripts/d3trial.js
@@ -1,27 +1,40 @@
-var width = 960,
-    height = 500;
+d3.select(window)
+        .on("resize", sizeChange);
 
-var projection = d3.geo.mercator()
-    .center([0, 5 ])
-    // .scale(900)
-    .rotate([-180,0]);
+var projection = d3.geo.mercator();
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+var svg = d3.select("#container")
+    .append("svg")
+    .append("g");
 
 var path = d3.geo.path()
     .projection(projection);
 
 
+
+
 // load and display the World
 d3.json("/assets/worldcountries.geo.json", function(topology) {
-    svg.selectAll("path")
+    // var zoom = d3.behavior.zoom()
+    //   .translate([0, 0])
+    //   .scale(1)
+    //   .scaleExtent([1, 8])
+    //   .on("zoom", zoomed);
+    svg.selectAll("country")
       .data([topology.features[0]])
     .enter()
       .append("path")
-      .attr("d", path)
-      .attr('fill', 'white')
-      .attr('stroke', 'black')
-      .attr('stroke-width', .5);
+      .attr("class", "country")
+      .attr("d", path);
 });
+function sizeChange() {
+      d3.select("g").attr("transform", "scale(" + $("#container").width()/900 + ")");
+      $("svg").height($("#container").width()*0.618);
+  }
+
+// function zoomed() {
+//     features.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+//     features.select(".state-border").style("stroke-width", 1.5 / d3.event.scale + "px");
+//     features.select(".county-border").style("stroke-width", .5 / d3.event.scale + "px");
+// }
+

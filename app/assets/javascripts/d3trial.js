31d4bc2d004f3b910c50672b4be7b80de716d9d9
nate dawg <nglass1988@gmail.com>
2013-08-07 15:36:43 -0700

    changed d3.json to ajax and things are working

--- app/assets/javascripts/d3trial.js
+++ app/assets/javascripts/d3trial.js
@@ -1,36 +1,48 @@
 $(document).ready(function() {
+
   $(window).keydown(function(event){
     if(event.keyCode == 13) {
       event.preventDefault();
       return false;
     }
   });
-});
 
-$("#target").keyup(function() {
+$("#myInput").keyup(function() {
   var result = $('#myInput').val();  
-  console.log(result);
 
 
 
 // load and display the World
-  d3.json("/assets/worldcountries.geo.json", function(topology) {
+  $.ajax({
+    url: '/assets/worldcountries.geo.json',
+    dataType: 'json',
+    success: function(data) {
+    console.log(data);
+      // write code to parse the JSON portion of the response and save it in a variable called topology
+    //    console.log(topology);
       
-    for (i=0; i < topology.features.length; i++)
-    {
-      if (topology.features[i].properties.name == result)
+    for (i=0; i < data.features.length; i++) {
+
+    
+      if (data.features[i].properties.name == result)
         { 
           $("svg").remove();
-          var found = topology.features[i].geometry; 
-          makecunts();}
+          var found = data.features[i].geometry; 
+          makecunts();
+        }
     }
     function makecunts() {
+      var width = $(window).width(),
+          height = ($(window).height());
       // $('#target').addClass('hidden');
-      var projection = d3.geo.mercator();
+      var projection = d3.geo.mercator()
+        .translate([width/2, height/2])
+        .center([0, 0]);;
 
       var svg = d3.select("#container")
           .append("svg")
-          .append("g");
+          .attr('width', $(window).width())
+          .attr('height', $(window).height());
 
       var path = d3.geo.path()
           .projection(projection);
@@ -49,12 +61,15 @@ $("#target").keyup(function() {
     }
 //       .transition()
 //       .duration(1500)
+
 //       .delay(1000).attr("transform", "scale(" + k + ")translate(" + x + width/2 + "," + y + ")")
 // ;
-
+    }
   });
   return false;
 });
+});
+
   // d3.select(window)
   //             .on("resize", sizeChange);
 

231d9b3dd0ad5575ce334a02ac56bb8b384daea2
nate dawg <nglass1988@gmail.com>
2013-08-13 10:33:19 -0700

    starting login

--- app/assets/javascripts/d3trial.js
+++ app/assets/javascripts/d3trial.js
@@ -7,9 +7,24 @@ $(document).ready(function() {
     }
   });
 
+  $("#login").on("click", function() {
+    $("#login").remove();
+  });
+
+  var width = $(window).width(),
+      height = ($(window).height()),
+      centered;
+     
+
+  var svg = d3.select("#container")
+      .append("svg")
+      .attr('width', width)
+      .attr('height', height);
+
+
+
 $("#myInput").keyup(function() {
   var result = $('#myInput').val();  
-
 
 
 // load and display the World
@@ -17,40 +32,36 @@ $("#myInput").keyup(function() {
     url: '/assets/worldcountries.geo.json',
     dataType: 'json',
     success: function(data) {
-    console.log(data);
+    console.log("sup");
       // write code to parse the JSON portion of the response and save it in a variable called topology
     //    console.log(topology);
       
     for (i=0; i < data.features.length; i++) {
 
-    
-      if (data.features[i].properties.name == result)
+      
+      if (data.features[i].properties.name == result && $("." + data.features[i].properties.name.toLowerCase()).length == 0)
         { 
-          $("svg").remove();
+          $(".active").remove();
           var found = data.features[i].geometry; 
+          
+
+
+        
           makecunts();
         }
     }
+
     function makecunts() {
-      var width = $(window).width(),
-          height = ($(window).height()),
-          centered;
-      // $('#target').addClass('hidden');
+      
+      var g = svg.append("g").attr("stroke-width", 1.5);
       var projection = d3.geo.mercator()
         .translate([width/2, height/2])
         .center([0, 0])
         .scale(196);
 
-
-      var svg = d3.select("#container")
-          .append("svg")
-          .attr('width', width)
-          .attr('height', height);
-
-      var g = svg.append("g").attr("stroke-width", 1.5);
-
       var path = d3.geo.path()
           .projection(projection);
+      
       
       //make the country
       g.append("g")
@@ -59,69 +70,43 @@ $("#myInput").keyup(function() {
         .data([found])
         .enter()
         .append("path")
-        .attr("class", "country")
+        .attr("class", data.features[i].properties.name.toLowerCase())
         .attr("d", path);
     
       clicked(found);
 
-
+data.features[i].properties.name.toLowerCase()
         function clicked(d) {
 
           var x, y, k;
 
-          if (d && centered !== d) {
+          // if (d && centered !== d) {
             var centroid = path.centroid(d);
             console.log(centroid);
             x = centroid[0];
             y = centroid[1];
             k = 4;
             centered = d;
-          } else {
-            x = width / 2;
-            y = height / 2;
-            console.log(x);
-                        console.log(y);
+          // } else {
+            // x = width / 2;
+            // y = height / 2;
+            // console.log(x);
+            //             console.log(y);
 
-            k = 1;
-            centered = null;
-          }
+            // k = 1;
+            // centered = null;
+          // }
           console.log(x);
 
           g.selectAll("path")
               .classed("active", centered && function(d) { return d === centered; });
 
           g.transition()
-              .duration(500)
-              .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+              .duration(1500)
+              .attr("transform", "translate(" + width / 2 + "," + height / 3 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
               .style("stroke-width", 1.5 / k + "px");
+          svg.selectAll('.active').transition().delay(300).duration(1500).style('fill', 'lightblue')
         }
-
-
-      // projection = d3.geo.mercator()
-      //   .translate([width/2, height/2])
-      //   .center(projection.invert(path.centroid(found)))
-      //   .scale(196);
-
-      console.log(projection);
-       // projection.center = projection.invert(path.centroid(found));
-        // var pixtolatlon = projection.invert(path.centroid(found));
-        // console.log(projection.);
-      console.log(path.centroid(found));
-
-        // projection.center(pixtolatlon);
-
-//       getCentroid(cunt);
-//       function getCentroid(cunt) {
-//       console.log(cunt);
-//     // get the DOM element from a D3 selection
-//     // you could also use "this" inside .each()
-     
-//         // use the native SVG interface to get the bounding box
-//         bbox = cunt.getBBox();
-//     // return the center of the bounding box
-//       return [bbox.x + bbox.width/2, bbox.y + bbox.height/2];
-// }
-     
     }
 //       .transition()
 //       .duration(1500)
---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe 'Home Page' do
+	before { visit root_path }
+	it "should have content" do
+		expect(page).to have_field("Type a Country");
+	end
+end

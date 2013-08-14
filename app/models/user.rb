4317b29b4b4bfa942ea05d1cbb6fcde89d20b58f
nate dawg <nglass1988@gmail.com>
2013-08-14 15:31:13 -0700

    generated country model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User
   include MongoMapper::Document
+  # { validate :name, presence: true
 
   key :name, String
   key :email, String
 
+
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_length_of :name, :within => 5..20
 end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -3,8 +3,17 @@ require 'spec_helper'
 describe 'Home Page' do
 	before { visit root_path }
 	it "should have content" do
-		expect(page).to have_field("Type a Country");
+		expect(page).to have_field("Type a Country")
 	end
+	
+	it 'routes /users/new to users#new' do
+  expect(get: '/users/new').to route_to(
+      controller: 'users',
+      action: 'new'
+    )
+	end
+	
 end
+
 
 
